5655a6c507b1ed1986fff2ab9f9d2b0bdaafce9b
xavier77dev <xavier.basurto.77@gmail.com>
2025-02-13 20:45:14 -0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //declaration and assignment var
+
+        int age = 77;
+        double priceTotal = 1.71;
+        boolean isActive = true;
+        char letter = 'a';
+        String description = "Hello World, Java";
+        long amountPeople = 975454545L;
+
+        System.out.println("My age:" + age);
+
+    }
+}
